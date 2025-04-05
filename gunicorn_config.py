a559bf58165107b1ce2508077c737020a0050474
Flavio De Stefano <destefano.flavio@gmail.com>
2025-04-05 23:27:05 +0200

    Increase timeout grace

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,20 +2,18 @@ import multiprocessing
 
 # Gunicorn config variables
 bind = "0.0.0.0:8000"
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = multiprocessing.cpu_count()
 worker_class = "sync"
 worker_connections = 1000
-timeout = 120
+timeout = 600
 keepalive = 2
 errorlog = "-"
 loglevel = "info"
 accesslog = "-"
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
-
-# Production optimizations
 max_requests = 1000
 max_requests_jitter = 50
-graceful_timeout = 30
+graceful_timeout = 600
 preload_app = True
 forwarded_allow_ips = '*'
 proxy_protocol = True
